cdd227e3bc5014b6a869ae14f94c916df90e3c82
Killerkoas <31402929+Killerkoas@users.noreply.github.com>
2018-02-19 12:12:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -226,7 +226,7 @@ var user = msg.mentions.users.first();
 	'ping': (msg) => {
 	var initTime = new Date(msg.timestamp)
     msg.reply('Pong!').then((m) => {
-      m.edit('Pong ! :ping_pong: | *' + (new Date(message.timestamp) - new Date(msg.timestamp)) + '*ms)
+      m.edit('Pong ! :ping_pong: | *' + (new Date(message.timestamp) - new Date(msg.timestamp)) + '*ms')
     })
 	}
 };
