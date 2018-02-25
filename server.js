8cf98c4bbc99bebdcf49b0e70ef61979f39577d6
Killerkoas <31402929+Killerkoas@users.noreply.github.com>
2018-02-25 09:07:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -345,14 +345,13 @@ let embed_fields = [{
   startTime = Date.now();
   msg.channel.sendMessage("Calcul en cours...").then((message) => {
     endTime = Date.now();
-     message.edit("Message : " + Math.round(endTime - startTime) + " ms\nAPI : "+Math.round(bot.ping)+" ms");
-  })	
+     message.edit("Bot : " + Math.round(endTime - startTime) + " ms\nAPI : "+Math.round(bot.ping)+" ms");
+  });
 	},
 	'setgame': (msg) => {
 	    if (msg.author.id !== "242682458083033089") return msg.channel.sendMessage(":no_entry_sign: Vous n'avez pas accès à cette commande ! :no_entry_sign:");
   	msg.channel.send("Setting updated : Setgame")
     client.user.setGame(`kd!help | ${client.guilds.size} serveurs | Beta`);
-  }
   	}
 	
 };      
