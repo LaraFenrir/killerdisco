e747c21239b6470babf97036a072f9474218a403
Killerkoas <31402929+Killerkoas@users.noreply.github.com>
2018-03-05 23:46:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -361,29 +361,6 @@ let embed_fields = [{
      }
      msg.channel.sendFile(r.uri.href);
     });
-	},
-	'dog': (msg) => {
-		var request = require('superagent')
-request.get('https://random.dog/woof.json')
-    .end((err, res) => {
-      if (!err && res.status === 200) {
-        msg.channel.sendMessage(res.body.url)
-      } else {
-        console.log(`Erreur, ${err}, status code: ${res.status}`)
-      }
-    })	
-	},
-	'hug': (msg) => {
-	const hug = ["https://s-media-cache-ak0.pinimg.com/originals/49/a2/1e/49a21e182fcdfb3e96cc9d9421f8ee3f.gif", "https://s-media-cache-ak0.pinimg.com/originals/16/46/f7/1646f720af76ea58853ef231028bafb1.gif", "https://media.giphy.com/media/xJlOdEYy0r7ZS/giphy.gif", "http://i.imgur.com/2WywS3T.gif", "http://i.imgur.com/8ruodNJ.gif", "https://myanimelist.cdn-dena.com/s/common/uploaded_files/1461071296-7451c05f5aae134e2cceb276b085a871.gif", "http://i0.kym-cdn.com/photos/images/original/000/931/030/394.gif", "https://media.tenor.co/images/1171c186f9130d1efa4a186ad4371e8c/tenor.gif", "http://cdn.smosh.com/sites/default/files/ftpuploads/bloguploads/0413/epic-hugs-friends-pikachu.gif"]
-const rn = require('random-number')
-    if (!msg.mentions.users.first()) return msg.channel.send("Bon, tu peux pas faire un calin dans le vide à part si tu es fou ... :neutral_face:");
-    let r = rn({
-        min: 0,
-        max: hug.length - 1,
-        integer: true
-    });
-    let image = hug[r];
-    msg.channel.send('**' + msg.author.username + '** *fait un calin à* **' + msg.mentions.users.first().username + '** :heart:\n' + image);
 	}
 	
 };      
