a5c209dcb49133aa451f12bce70eb3dbd9f8c355
Certurix <31402929+Certurix@users.noreply.github.com>
2018-04-27 21:52:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -450,21 +450,6 @@ function clean(text) {
             color: 0x4077FF
           }});    
      },
-    'hug': (msg) => {
-	const hug = ["https://s-media-cache-ak0.pinimg.com/originals/49/a2/1e/49a21e182fcdfb3e96cc9d9421f8ee3f.gif", "https://s-media-cache-ak0.pinimg.com/originals/16/46/f7/1646f720af76ea58853ef231028bafb1.gif", "https://media.giphy.com/media/xJlOdEYy0r7ZS/giphy.gif", "http://i.imgur.com/2WywS3T.gif", "http://i.imgur.com/8ruodNJ.gif", "https://myanimelist.cdn-dena.com/s/common/uploaded_files/1461071296-7451c05f5aae134e2cceb276b085a871.gif", "http://i0.kym-cdn.com/photos/images/original/000/931/030/394.gif", "https://media.tenor.co/images/1171c186f9130d1efa4a186ad4371e8c/tenor.gif", "http://cdn.smosh.com/sites/default/files/ftpuploads/bloguploads/0413/epic-hugs-friends-pikachu.gif"]
-const rn = require('random-number')
-
-module.exports.func = function (bot, message, guild) {
-    if (!msg.mentions.users.first()) return msg.channel.send(":warning: Vous devez mentionner quelqu'un.");
-    let r = rn({
-        min: 0,
-        max: hug.length - 1,
-        integer: true
-    });
-    let image = hug[r];
-    msg.channel.send('**' + msg.author.username + '** *fait un calin à* **' + msg.mentions.users.first().username + '** :heart:\n' + image);
-}    
-    },
 	
 };      
 	
